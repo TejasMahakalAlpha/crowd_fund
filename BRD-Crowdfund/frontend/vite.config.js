2a84bdcc5f69fb87ca9dc985c78f8fc5227a1547
NitinAlphaseam <nitinsonawane@alphaseam.com>
2025-08-28 15:14:14 +0530

    Changes to build file

--- BRD-Crowdfund/frontend/vite.config.js
+++ BRD-Crowdfund/frontend/vite.config.js
@@ -3,4 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // increase from default 500kB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Split big dependencies into separate chunks
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('swiper')) return 'swiper-vendor';
+            if (id.includes('axios')) return 'axios-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
